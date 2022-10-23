b73c1a4c209ef6874373363877ed01650f595907
Pranav <pranavshelke4@gmail.com>
2022-10-23 13:39:51 +0530

    Routes done

---
+++ client/src/component/Signup.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+function Signup() {
+  return (
+    <div>
+      {" "}
+      <Navbar />
+      Signup
+    </div>
+  );
+}
+
+export default Signup;
